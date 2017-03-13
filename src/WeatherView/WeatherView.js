8b18a7989b2c65f09e09d56292b427925d2b6406
alex-skiffin <alex@skiffin.ru>
2017-03-13 23:40:45 +0500

    added receiving of weather data

--- src/WeatherView/WeatherView.js
+++ src/WeatherView/WeatherView.js
@@ -6,18 +6,33 @@ import store from '../localStore';
 class WeatherView extends Component {
     constructor(props) {
         super(props);
-        this.state = { selectedCity: '' };
+        this.state = { selectedCity: '', weatherJson: '' };
         store.subscribe(this.handleChange.bind(this));
-    } 
+    }
     handleChange() {
         this.setSelectedCity(this.props.localState.selectedCity);
     }
     setSelectedCity(city) {
         this.setState({ selectedCity: city });
+        let myRequest = new Request('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&APPID=');
+        let req = fetch(myRequest)
+            .then(function (response) {
+                if (response.status === 200)
+                    return response.json();//this.setState({ weatherJson: response.json() });
+            })
+            .catch(function (error) {
+                console.error(error);
+            });
+        req.then((bd) => {
+            this.setState({ weatherJson: JSON.stringify(bd.weather) });
+        });
     }
     render() {
         return (
-            <div className="Weather-view" >weather in {this.state.selectedCity} view will be here soon</div>
+            <div>
+                <div className="Weather-view" >weather in {this.state.selectedCity} view will be here soon</div>
+                <div>{this.state.weatherJson}</div>
+            </div>
         )
     };
 }
